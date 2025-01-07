4cd1334353715dbdf70718a847733684e10980bb
Eugen Hruska <9983894+euhruska@users.noreply.github.com>
2025-01-07 17:17:17 +0100

    add preprint, formating

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@ pdflatex Hruska-CV
 biber Hruska-CV
 pdflatex Hruska-CV
 pdflatex Hruska-CV
-pdflatex Hruska-resume
-biber Hruska-resume
-pdflatex Hruska-resume
-pdflatex Hruska-resume
+pdflatex Hruska-CV-2page
+biber Hruska-CV-2page
+pdflatex Hruska-CV-2page
+pdflatex Hruska-CV-2page
 pdflatex Hruska-bibliography
 biber Hruska-bibliography
 pdflatex Hruska-bibliography
@@ -14,7 +14,4 @@ pdflatex Hruska-bibliography-abstracts
 biber Hruska-bibliography-abstracts
 pdflatex Hruska-bibliography-abstracts
 pdflatex Hruska-bibliography-abstracts
-pdflatex Hruska-CV-nopub
-biber Hruska-CV-nopub
-pdflatex Hruska-CV-nopub
-pdflatex Hruska-CV-nopub
+
